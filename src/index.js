5ead7c1986953007b78ca385e3cbad07df86c69e
[vicdaner] <[vicdaner@gmail.com]>
2021-10-19 22:38:46 +0200

    primer commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import './styles.css';
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes'
+
+export  const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml( todo ) );
+        // const tarea = new Todo('aprender js');
+      //  todoList.todos[0].imprimirClase();
+console.log('todos',todoList.todos);
+// todoList.nuevoTodo(tarea);
+// console.log(todoList);
+
+// crearTodoHtml( tarea );
